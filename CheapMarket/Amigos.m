74348f15ae799d0aaa632b50e2c37d07f3fa9e7e
conradowerlang <conradowerlang@Conrado.local>
2015-03-09 10:28:47 -0300

    Commit Amigos.m

---
+++ CheapMarket/Amigos.m
@@ -0,0 +1,24 @@
+//
+//  Amigos.m
+//  CheapMarket
+//
+//  Created by Marco Aurelio Dias Americo on 3/9/15.
+//  Copyright (c) 2015 Marco Aurelio Dias Americo. All rights reserved.
+//
+
+#import "Amigos.h"
+
+@implementation Amigos
+
+#pragma mark - Metodos da Classe
+
+-(void) adciona
+{
+    
+}
+-(void) remove
+{
+
+}
+
+@end
